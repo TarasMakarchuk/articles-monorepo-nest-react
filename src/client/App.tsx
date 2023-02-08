60e175b16a47b01da5e4f7a1d49b0ce2dc71d82d
Taras Makarchuk <2278452@gmail.com>
2023-02-08 18:41:59 +0200

    :construction: add webpack, webpack-cli, react, react-dom npms, create index.html, create webpack.config.js, add jsx dialect to tsconfig.json, add "build:client" to package.json

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,1 +1,8 @@
-const a = 1;
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+    return <h1>Hello app</h1>;
+};
+
+render(<App />, document.getElementById('app'));
