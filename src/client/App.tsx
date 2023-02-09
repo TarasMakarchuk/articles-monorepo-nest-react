c7f8536603396adbfb09800119e4150b49810cc3
Taras Makarchuk <2278452@gmail.com>
2023-02-09 18:29:57 +0200

    :white_check_mark: change settings in Dockerfile, update "build:client" command in the package.json, receive index.html on 127.0.0.1/index.html

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { render } from 'react-dom';
 
 const App = () => {
-    return <h1>Hello app</h1>;
+    return <h1>Hello app React!</h1>;
 };
 
 render(<App />, document.getElementById('app'));
