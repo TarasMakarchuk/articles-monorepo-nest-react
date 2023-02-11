0387521c2589bc48f41ce7f1dfa0bdfc80a2f61b
Taras Makarchuk <2278452@gmail.com>
2023-02-11 17:48:36 +0200

    :white_check_mark: add routes and article list

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,8 +1,23 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { ArticleList } from './ArticleList';
 
 const App = () => {
-    return <h2><a href="/">Main</a></h2>;
+    return (
+        <BrowserRouter>
+        <h2><a href="/">Main</a></h2>
+            <nav>
+                <li>
+                    <Link to='/article'>Articles</Link>
+                </li>
+            </nav>
+
+            <Routes>
+                <Route path='/article' element={ <ArticleList /> } />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 render(<App />, document.getElementById('app'));
--- src/server/app.controller.ts
+++ src/server/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Render } from '@nestjs/common';
 @Controller('/')
 export class AppController {
 
-    @Get([''])
+    @Get(['', 'article'])
     @Render('layout')
     pages() {
 
